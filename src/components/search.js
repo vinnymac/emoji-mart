e9a92f76ebe659a18969e2e71a9e69d912453aa1
Vincent Taverna <vinny@getlua.com>
2017-04-04 11:11:23 -0400

    use callback ref for search

--- src/components/search.js
+++ src/components/search.js
@@ -3,21 +3,20 @@ import emojiIndex from '../utils/emoji-index'
 
 export default class Search extends React.Component {
   handleChange() {
-    var { input } = this.refs,
-        value = input.value
+    var value = this.input.value
 
     this.props.onSearch(emojiIndex.search(value, this.props.emojisToShowFilter, this.props.maxResults))
   }
 
   clear() {
-    this.refs.input.value = ''
+    this.input.value = ''
   }
 
   render() {
     var { i18n } = this.props
 
     return <input
-      ref='input'
+      ref={(c) => { this.input = c }}
       type='text'
       onChange={this.handleChange.bind(this)}
       placeholder={i18n.search}
